CyanogenMod/android_frameworks_base

Support Motorola Custom libcamera Parameters
DO NOT MERGE, UNTESTED

Change-Id: I30bfc02862972a9437e925322ab0e7ea89d4a767

libs/camera/Android.mk
@@ -22,6 +22,10 @@ ifeq ($(BOARD_CAMERA_USE_GETBUFFERINFO),true)
     LOCAL_CFLAGS += -DUSE_GETBUFFERINFO
 endif
 
+ifeq ($(TARGET_USE_MOTO_CUSTOM_PARAMETERS),true)
+    LOCAL_CFLAGS += -DMOTO_CUSTOM_PARAMETERS
+endif
+
 ifeq ($(TARGET_SIMULATOR),true)
     LOCAL_LDLIBS += -lpthread
 endif

libs/camera/Camera.cpp
@@ -305,6 +305,17 @@ status_t Camera::setParameters(const String8& params)
     return c->setParameters(params);
 }
 
+#ifdef MOTO_CUSTOM_PARAMETERS
+// set preview/capture parameters - key/value pairs
+status_t Camera::setCustomParameters(const String8& params)
+{
+    LOGV("setCustomParameters");
+    sp <ICamera> c = mCamera;
+    if (c == 0) return NO_INIT;
+    return c->setCustomParameters(params);
+}
+#endif
+
 // get preview/capture parameters - key/value pairs
 String8 Camera::getParameters() const
 {
@@ -314,6 +325,18 @@ String8 Camera::getParameters() const
     if (c != 0) params = mCamera->getParameters();
     return params;
 }
+
+#ifdef MOTO_CUSTOM_PARAMETERS
+// get preview/capture parameters - key/value pairs
+String8 Camera::getCustomParameters() const
+{
+    LOGV("getCustomParameters");
+    String8 params;
+    sp <ICamera> c = mCamera;
+    if (c != 0) params = mCamera->getCustomParameters();
+    return params;
+}
+#endif
 
 // send command to camera driver
 status_t Camera::sendCommand(int32_t cmd, int32_t arg1, int32_t arg2)

atrix-aosp/frameworks_base

libs/camera/Android.mk
@@ -18,6 +18,10 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_MODULE:= libcamera_client
 
+ifeq ($(TARGET_USE_MOTO_CUSTOM_CAMERA_PARAMETERS),true)
+    LOCAL_CFLAGS += -DMOTO_CUSTOM_PARAMETERS
+endif
+
 ifeq ($(TARGET_SIMULATOR),true)
     LOCAL_LDLIBS += -lpthread
 endif

libs/camera/Camera.cpp
@@ -294,6 +294,17 @@ status_t Camera::setParameters(const String8& params)
     return c->setParameters(params);
 }
 
+#ifdef MOTO_CUSTOM_PARAMETERS
+// set preview/capture parameters - key/value pairs
+status_t Camera::setCustomParameters(const String8& params)
+{
+    LOGV("setCustomParameters");
+    sp <ICamera> c = mCamera;
+    if (c == 0) return NO_INIT;
+    return c->setParameters(params);
+}
+#endif
+
 // get preview/capture parameters - key/value pairs
 String8 Camera::getParameters() const
 {
@@ -303,6 +314,18 @@ String8 Camera::getParameters() const
     if (c != 0) params = mCamera->getParameters();
     return params;
 }
+
+#ifdef MOTO_CUSTOM_PARAMETERS
+// get preview/capture parameters - key/value pairs
+String8 Camera::getCustomParameters() const
+{
+    LOGV("getCustomParameters");
+    String8 params;
+    sp <ICamera> c = mCamera;
+    if (c != 0) params = mCamera->getParameters();
+    return params;
+}
+#endif
 
 // send command to camera driver
 status_t Camera::sendCommand(int32_t cmd, int32_t arg1, int32_t arg2)